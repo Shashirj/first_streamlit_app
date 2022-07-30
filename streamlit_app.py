a6f4b1dc96ddef2de02ed17f6c224f471a06dbad
Shashirj <84465032+Shashirj@users.noreply.github.com>
2022-07-30 15:09:08 +0530

    Update streamlit_app.py
    .loc function added

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,6 @@ streamlit.header("🍌🍓Build Your Own Fruit Smoothie🥝🍇")
 import pandas as pd
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_to_show=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),["Avocado","Banana"])
+fruits_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),["Avocado","Banana"])
+fruits_to_show=my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
